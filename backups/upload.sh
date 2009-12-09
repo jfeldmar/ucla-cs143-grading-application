d1fb7ec8727b2f687a9c43d6f15b177c99308bce
juliausko <juliausko@84370d8c-cdbb-11de-958f-af26545d0b1a>
2009-12-09 00:00:23 +0000

    removing unneccessary directories (notes files, etc.)

--- backups/upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-if [ $# -eq 2 ]
-then
-#	echo "python googlecode_upload.py  -s \"$1\" -p cs143-grading-application -u juliausko -w bZ9Cc3yy6pg3 $2"
-	python googlecode_upload.py  -s "$1" -p cs143-grading-application -u juliausko -w bZ9Cc3yy6pg3 $2
-else
-	echo "Two Parameters required: Summary and File Name"
-fi
--- notes/proj1a/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /var/www/p1a/src
-xterm  -e 'ls && bash' &
-
-exit
